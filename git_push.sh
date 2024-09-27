a15ba2c7c9289d83bb99e366e80c275f0f3136aa
Sahutsawat Sivakosit <aong1244@gmail.com>
2024-09-27 23:17:38 +0700

    update yml file

---
+++ git_push.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Add all changes
+git add .
+
+# Commit with a message
+git commit -m "update yml file"  # You can parameterize this if you want to pass a message as an argument.
+
+# Push to the remote repository
+git push -u origin master  # Change "master" to your current branch if needed
