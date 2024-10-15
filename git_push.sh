c461c5dcd51a4bb5b0c03705ceaf50caacb59441
Sahutsawat Sivakosit <aong1244@gmail.com>
2024-10-16 01:57:09 +0700

    Change repository from master to main on github workflow

--- git_push.sh
+++ git_push.sh
@@ -4,7 +4,7 @@
 git add .
 
 # Commit with a message
-git commit -m "Validate the user input both backend or frontend. Dompurify for frontend and validator for backend. Improve UI for editProduct page"  # You can parameterize this if you want to pass a message as an argument.
+git commit -m "Change repository from master to main on github workflow"  # You can parameterize this if you want to pass a message as an argument.
 
 # Push to the remote repository
 git push -u origin master  # Change "master" to your current branch if needed
